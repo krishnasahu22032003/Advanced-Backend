552aae72520c149f75a2f1caafca00c28e86ab86
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-26 03:06:17 +0530

    basic llm calling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express,{ type Request, type Response } from "express";
+import { GoogleGenAI } from "@google/genai";
+import "dotenv/config";
+
+const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY as string});
 
 const app = express();
 
@@ -6,9 +10,27 @@ app.use(express.json());
 
 const PORT = 3000
 
-app.get("/", (req: Request, res: Response) => {
+app.post("/generate", async (req: Request, res: Response) => {
 
-res.send("Testing...")
+const{input} = req.body ; 
+
+try{
+
+const response = await ai.models.generateContent({
+    model:"gemini-2.5-flash",
+   contents:input,
+   config:{
+    systemInstruction:"you are a assistant and your name is jarvis , don't give wrong answer if you don't know the right answer just say i don't know "
+}}) 
+console.log(response.text) ;
+
+return res.status(200).json({
+    message:"success",
+    data:response.text
+})
+}catch(error){
+    console.log(error)
+}
 
 })
 
