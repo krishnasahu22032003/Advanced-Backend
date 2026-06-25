cc255693fcc2f7ac758ca6ce9581e737606b3f61
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-25 22:58:19 +0530

    Express init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world") ;
+import express,{ type Request, type Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000
+
+app.get("/", (req: Request, res: Response) => {
+
+res.send("Testing...")
+
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+})
