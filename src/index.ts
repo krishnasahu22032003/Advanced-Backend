d1e3eddd7162cfda5d7e5a2666eabf71068877ac
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-26 19:46:14 +0530

    tool

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express, { application, type Request, type Response } from "express";
 import { GoogleGenAI } from "@google/genai";
 import "dotenv/config";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
-import { Annotation, StateGraph } from "@langchain/langgraph";
+import { Annotation, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
 
 const app = express();
 
@@ -43,68 +44,89 @@ const PORT = 3000
 
 //state where we store the prompt 
 
-const state = Annotation.Root({
+// const state = Annotation.Root({
 
-    prompt: Annotation,
-    aiMessage: Annotation,
+//     prompt: Annotation,
+//     aiMessage: Annotation,
 
-});
+// });
 
 // function for the agent node 
+
+//create tool using tevily search
+
+const tool = new TavilySearch({
+  maxResults: 5,
+  topic: "general",
+});
+
+//To save 
+
+//Creating tool node for the conditional other service search 
+
+const tools: any = [tool];
+
+const toolNode = new ToolNode(tools);
+
 
 const llm = new ChatGoogleGenerativeAI({
     model: "gemini-2.5-flash",
     temperature: 0, // by default 0.7,
     maxRetries: 2,//by default 6
-})
+}).bindTools(tools)
 
 const callLLM = async (state: any) => {
 
-    console.log("state" , state)
+    console.log("state", state)
 
     const response = await llm.invoke([
         { role: "system", content: "You are personal assistant jarvis greet every time i ask something" },
-        { role: "human", content: state.prompt }
+   ...state.messages
     ]);
 
-    return { aiMessage: response.content }
+    return { messages: [response] }
 
 };
 
-//Creating tool node for the conditional other service search 
 
-    const tools:any = [] ;
-
-    const toolNode = new ToolNode(tools) ;
-
-    const shouldContinue = async()=>{
-        
+const shouldContinue = async (state: any) => {
+    const lastMessage = state.messages[state.messages.length - 1]
+    if (lastMessage.tool_calls.length > 0) {
+        return "tool"
+    } else {
+        return "__end__"
     }
+}
 
 // create a graph so that we can create nodes and edges 
 
-const graph = new StateGraph(state)
+const graph = new StateGraph(MessagesAnnotation)
     .addNode("agent", callLLM)
-    .addNode("tool" , toolNode)
-    .addEdge("__start__" , "agent")
-    .addEdge("tool" ,"agent")
-    .addConditionalEdges("agent" , shouldContinue)
-    .addEdge("agent" ,"__end__")
+    .addNode("tool", toolNode)
+    .addEdge("__start__", "agent")
+    .addEdge("tool", "agent")
+    .addConditionalEdges("agent", shouldContinue)
+    .addEdge("tool", "agent")
     .compile()
 
 
 
-app.post("/generate" , async (req:Request , res:Response)=>{
+app.post("/generate", async (req: Request, res: Response) => {
 
-    const {input} =  req.body ; 
+    const { input } = req.body;
 
-    const response = await graph.invoke({prompt:input});
+    const response = await graph.invoke({
+        messages: [{
+            role: "user",
+            content: input
+        }]
+    });
 
-    console.log(response) ; 
+    console.log(response);
 
     return res.status(200).json({
-        success:true ,
-        data:response 
+        success: true,
+        data: response.messages[response.messages.length - 1]?.content
     })
 })
 //with langChain 
