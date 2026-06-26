83ad1669f45e5a2a5e5bcc8541763e3aec6dcf1d
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-26 17:44:08 +0530

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import express, { type Request, type Response } from "express";
+import express, { application, type Request, type Response } from "express";
 import { GoogleGenAI } from "@google/genai";
 import "dotenv/config";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { Annotation, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 
 const app = express();
 
@@ -37,31 +39,99 @@ const PORT = 3000
 
 // })
 
-//with langChain 
+// LangGraph
 
-app.post("/langchain/generate", async (req: Request, res: Response) => {
+//state where we store the prompt 
 
-    const { input } = req.body;
+const state = Annotation.Root({
 
-    const llm = new ChatGoogleGenerativeAI({
-        model: "gemini-2.5-flash",
-        temperature: 0, // by default 0.7,
-        maxRetries: 2,//by default 6
-    })
+    prompt: Annotation,
+    aiMessage: Annotation,
+
+});
+
+// function for the agent node 
+
+const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    temperature: 0, // by default 0.7,
+    maxRetries: 2,//by default 6
+})
+
+const callLLM = async (state: any) => {
+
+    console.log("state" , state)
 
     const response = await llm.invoke([
         { role: "system", content: "You are personal assistant jarvis greet every time i ask something" },
-        { role: "human", content: input }
-    ])
+        { role: "human", content: state.prompt }
+    ]);
 
-    console.log(response.content)
+    return { aiMessage: response.content }
+
+};
+
+//Creating tool node for the conditional other service search 
+
+    const tools:any = [] ;
+
+    const toolNode = new ToolNode(tools) ;
+
+    const shouldContinue = async()=>{
+        
+    }
+
+// create a graph so that we can create nodes and edges 
+
+const graph = new StateGraph(state)
+    .addNode("agent", callLLM)
+    .addNode("tool" , toolNode)
+    .addEdge("__start__" , "agent")
+    .addEdge("tool" ,"agent")
+    .addConditionalEdges("agent" , shouldContinue)
+    .addEdge("agent" ,"__end__")
+    .compile()
+
+
+
+app.post("/generate" , async (req:Request , res:Response)=>{
+
+    const {input} =  req.body ; 
+
+    const response = await graph.invoke({prompt:input});
+
+    console.log(response) ; 
 
     return res.status(200).json({
-        success: true,
-        data: response.content
+        success:true ,
+        data:response 
     })
-
 })
+//with langChain 
+
+// app.post("/langchain/generate", async (req: Request, res: Response) => {
+
+//     const { input } = req.body;
+
+//     const llm = new ChatGoogleGenerativeAI({
+//         model: "gemini-2.5-flash",
+//         temperature: 0, // by default 0.7,
+//         maxRetries: 2,//by default 6
+//     })
+
+//     const response = await llm.invoke([
+//         { role: "system", content: "You are personal assistant jarvis greet every time i ask something" },
+//         { role: "human", content: input }
+//     ])
+
+//     console.log(response.content)
+
+//     return res.status(200).json({
+//         success: true,
+//         data: response.content
+//     })
+
+// })
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
